040d2107ea774b0c6ba2b05df8b568bdc0221127
Wenyu Huang <huangwenyuu@outlook.com>
2025-06-22 21:46:08 +0800

    Add gsoc-test
    Signed-off-by: Wenyu Huang <huangwenyuu@outlook.com>

---
+++ gsoc-test/src/backend.rs
@@ -0,0 +1,112 @@
+use libc::{iovec, read};
+use std::os::fd::RawFd;
+use std::os::unix::net::UnixListener;
+use std::path::Path;
+use std::{io, mem};
+use vmm_sys_util::epoll::{ControlOperation, Epoll, EpollEvent, EventSet};
+
+mod errno;
+mod sock_ctrl_msg;
+use sock_ctrl_msg::ScmSocket;
+
+struct Listener(UnixListener);
+
+impl Listener {
+    pub fn new<P: AsRef<Path>>(path: P) -> io::Result<Self> {
+        let _ = std::fs::remove_file(&path);
+        let fd = UnixListener::bind(&path)?;
+        Ok(Listener { 0: fd })
+    }
+}
+
+fn read_event(fd: RawFd) -> io::Result<()> {
+    let mut buf: u64 = 0;
+    let ret = unsafe {
+        read(
+            fd,
+            &mut buf as *mut u64 as *mut libc::c_void,
+            size_of::<u64>(),
+        )
+    };
+    if ret < 0 {
+        Err(io::Error::last_os_error())
+    } else {
+        Ok(())
+    }
+}
+
+fn main() {
+    println!("Backend: start");
+    let listener = Listener::new("vhost-fake.sck").expect("failed to create listener");
+
+    println!("Backend: waiting for connections");
+    let (socket_fd, _saddr) = listener.0.accept().expect("failed to accept connection");
+
+    // receive an FD for events
+    let mut files = [0; 2];
+    let mut buf = [0u8];
+    let mut iovecs = [iovec {
+        iov_base: buf.as_mut_ptr() as *mut libc::c_void,
+        iov_len: buf.len(),
+    }];
+
+    println!("Backend: waiting for FDs");
+    let (_read_count, file_count) = unsafe {
+        socket_fd
+            .recv_with_fds(&mut iovecs[..], &mut files)
+            .expect("failed to recv fd")
+    };
+
+    assert_eq!(file_count, 1, "checking the number of received fds");
+    assert!(files[0] >= 0);
+
+    let fd = files[0];
+    println!("Backend: waiting for events");
+    let epoll = Epoll::new().expect("failed to create epoll");
+    epoll
+        .ctl(
+            ControlOperation::Add,
+            fd,
+            EpollEvent::new(EventSet::IN, fd as u64),
+        )
+        .expect("failed to ctl epoll");
+
+    // let's poll the FD until is ready to be read
+    let mut num_evnt = 0;
+    const EPOLL_EVENTS_LEN: usize = 100;
+    let mut events = vec![EpollEvent::new(EventSet::empty(), 0); EPOLL_EVENTS_LEN];
+
+    'epoll: loop {
+        let num_events = match epoll.wait(-1, &mut events[..]) {
+            Ok(res) => res,
+            Err(e) => {
+                if e.kind() == io::ErrorKind::Interrupted {
+                    continue;
+                }
+                panic!("failed to wait for epoll: {}", e);
+            }
+        };
+
+        for event in events.iter().take(num_events) {
+            let _evset = match EventSet::from_bits(event.events) {
+                Some(evset) => evset,
+                None => {
+                    let evbits = event.events;
+                    println!("epoll: ignoring unknown event set: 0x{:x}", evbits);
+                    continue;
+                }
+            };
+
+            // we received and event in FD, we must read it
+            read_event(fd).expect("failed to read event");
+            println!("Backend: event {num_evnt} received.");
+            num_evnt += 1;
+
+            if num_evnt >= 3 {
+                break 'epoll;
+            }
+        }
+    }
+
+    println!("Backend: done.");
+}
---
+++ gsoc-test/src/frontend.rs
@@ -0,0 +1,60 @@
+use std::io;
+use std::os::fd::{AsRawFd, RawFd};
+use std::os::unix::net::UnixStream;
+use std::thread::sleep;
+use std::time::Duration;
+use vmm_sys_util::eventfd::EventFd;
+use vmm_sys_util::sock_ctrl_msg::ScmSocket;
+mod errno;
+mod sock_ctrl_msg;
+
+fn send_event(fd: RawFd) -> io::Result<()> {
+    let v = 1_u64;
+    let ret = unsafe {
+        libc::write(
+            fd,
+            &v as *const u64 as *const libc::c_void,
+            size_of::<u64>(),
+        )
+    };
+    if ret <= 0 {
+        Err(io::Error::last_os_error())
+    } else {
+        Ok(())
+    }
+}
+
+
+
+pub fn pipe() -> io::Result<(RawFd, RawFd)> {
+    let mut fds: [RawFd; 2] = [-1, -1];
+    let ret = unsafe { libc::pipe(fds.as_mut_ptr()) };
+    if ret == -1 {
+        Err(io::Error::last_os_error())
+    } else {
+        // read, write
+        Ok((fds[0], fds[1]))
+    }
+}
+
+
+fn main() {
+    println!("Frontend: start");
+
+    let (read_fd, write_fd) = pipe().expect("pipe failed");
+
+    println!("Frontend: connect and send FD");
+    let socket = UnixStream::connect("vhost-fake.sck").expect("failed to connect to frontend");
+    socket
+        .send_with_fds(&[[237].as_ref()], &[read_fd.as_raw_fd()])
+        .expect("failed to send FD");
+
+    for e in 0..3 {
+        sleep(Duration::from_secs(2));
+
+        println!("Frontend: send event: {e}");
+        send_event(write_fd.as_raw_fd()).expect("failed to send event");
+    }
+
+    println!("Frontend: done");
+}
